316a49989e269a3f32b3f7f2748336e6f964c206
nivaura-enrico.schintu <enrico.schintu@nivaura.com>
2020-10-09 08:53:49 +0200

    Added base assets and multi-language support

---
+++ es_musement_app/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'it',
+      fallbackLocale: 'en',
+      localeDir: '/assets/locales',
+      enableInSFC: true,
+    },
+  },
+};
